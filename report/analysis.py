7392062d09743aa7c747b19915429e5b6b5d5a59
Rodrigo Martins <45969994+rodrigomoliveira1@users.noreply.github.com>
2020-09-15 15:18:59 -0300

    Add full example to Report docstring
    Improving documentation

--- report/analysis.py
+++ report/analysis.py
@@ -56,9 +56,105 @@ class Report:
     Example
     -------
     >>> import ross as rs
+    >>> from ross.materials import steel
     >>> import report as rp
-    >>> rotor = rs.rotor_example()
-    >>> config = rp.Config()
+
+    >>> # Building the rotor model
+    >>> i_d = 0
+    >>> o_d = 0.05
+    >>> n = 6
+    >>> L = [0.25 for _ in range(n)]
+
+    # Shaft
+    >>> shaft_elem = [
+    ...     rs.ShaftElement(
+    ...         l,
+    ...         i_d,
+    ...         o_d,
+    ...         material=steel,
+    ...         shear_effects=True,
+    ...         rotary_inertia=True,
+    ...         gyroscopic=True,
+    ...     )
+    ...     for l in L
+    ... ]
+
+    # Disks
+    >>> disk0 = rs.DiskElement.from_geometry(
+    ...     n=2, material=steel, width=0.07, i_d=0.05, o_d=0.28
+    ... )
+    >>> disk1 = rs.DiskElement.from_geometry(
+    ...     n=4, material=steel, width=0.07, i_d=0.05, o_d=0.28
+    ... )
+
+    # Bearings
+    >>> stfx = [0.4e7, 0.5e7, 0.6e7, 0.7e7]
+    >>> stfy = [0.8e7, 0.9e7, 1.0e7, 1.1e7]
+    >>> freq = [400, 800, 1200, 1600]
+    >>> bearing0 = rs.BearingElement(0, kxx=stfx, kyy=stfy, cxx=2e3, frequency=freq)
+    >>> bearing1 = rs.BearingElement(6, kxx=stfx, kyy=stfy, cxx=2e3, frequency=freq)
+    >>> oper_clerance_brg = [bearing0, bearing1]
+
+    # Rotor
+    >>> rotor = Rotor(shaft_elem, [disk0, disk1], [bearing0, bearing1])
+
+    # coefficients for minimum clearance bearings
+    >>> stfx = [0.7e7, 0.8e7, 0.9e7, 1.0e7]
+    >>> dampx = [2.0e3, 1.9e3, 1.8e3, 1.7e3]
+    >>> freq = [400, 800, 1200, 1600]
+    >>> bearing0 = rs.BearingElement(0, kxx=stfx, cxx=dampx, frequency=freq)
+    >>> bearing1 = rs.BearingElement(6, kxx=stfx, cxx=dampx, frequency=freq)
+    >>> min_clearance_brg = [bearing0, bearing1]
+
+    # coefficients for maximum clearance bearings
+    >>> stfx = [0.4e7, 0.5e7, 0.6e7, 0.7e7]
+    >>> dampx = [2.8e3, 2.7e3, 2.6e3, 2.5e3]
+    >>> freq = [400, 800, 1200, 1600]
+    >>> bearing0 = rs.BearingElement(0, kxx=stfx, cxx=dampx, frequency=freq)
+    >>> bearing1 = rs.BearingElement(6, kxx=stfx, cxx=dampx, frequency=freq)
+    >>> max_clearance_brg = [bearing0, bearing1]
+
+    # Analyses setup
+    >>> config = Config()
+    >>> config.update_config(
+    ...     rotor_properties={
+    ...         "rotor_speeds": {
+    ...             "min_speed": 400,
+    ...             "max_speed": 1000,
+    ...             "oper_speed": 800,
+    ...             "trip_speed": 1200,
+    ...             "unit": "rad/s",
+    ...         }
+    ...     },
+    ...     bearings={
+    ...         "oper_clearance": oper_clerance_brg,
+    ...         "min_clearance": min_clearance_brg,
+    ...         "max_clearance": max_clearance_brg,
+    ...     },
+    ...     run_campbell={"speed_range": np.linspace(0, 1500, 51)},
+    ...     run_unbalance_response={
+    ...         "probes": {
+    ...             "node": [1, 4],
+    ...             "orientation": [np.pi / 2, np.pi / 2],
+    ...             "unit": "rad",
+    ...         },
+    ...         "frequency_range": np.linspace(0, 1500, 201),
+    ...         "plot_deflected_shape": {"speed": [615]},
+    ...     },
+    ...     plot_ucs={"stiffness_range": (5, 8)},
+    ...     stability_level1={
+    ...         "D": [0.35, 0.35],
+    ...         "H": [0.08, 0.08],
+    ...         "rated_power": [10000, 10000],
+    ...         "rho_ratio": [1.11, 1.14],
+    ...         "rho_suction": 30.45,
+    ...         "rho_discharge": 37.65,
+    ...         "length_unit": "m",
+    ...         "power_unit": "hp",
+    ...         "density_unit": "kg/m**3",
+    ...     },
+    ... )
+
     >>> report = rp.Report(rotor=rotor, config=config)
     >>> report.rotor_type
     'between_bearings'
@@ -1284,7 +1380,7 @@ class Report:
         >>> dataframe = report._stability_level_2()
         """
         oper_speed = self.config.rotor_properties.rotor_speeds.oper_speed
-        unit = speed_unit = self.config.rotor_properties.rotor_speeds.unit
+        unit = self.config.rotor_properties.rotor_speeds.unit
         oper_speed = Q_(oper_speed, unit).to("rad/s").m
 
         # Build a list of seals
